ce4facea200a17965b69bcb77ff76e8ea1b5fc5c
Jian Huang（BJ） <jhuang.abcft@niub.la>
2018-08-27 15:47:23 +0800

    [修改HashRouter]

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+// import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <Router basename="/">
+  <HashRouter basename="/">
     <App />
-  </Router>,
+  </HashRouter>,
   document.getElementById('root')
 )
 registerServiceWorker();
