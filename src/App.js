5fde1e454a42091a06b3f583fbd876ceb58555f2
黄健 <huangjian@huangjiandeMacBook-Pro.local>
2018-12-21 15:56:19 +0800

    [checkbocgroup]

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Login from './components/Login'
 import Home from './components/Home'
 import Detail from './components/Detail'
 import Hierarchy from './components/Hierarchy'
+import Dashboard from './components/Dashboard'
 import { Provider } from 'mobx-react'
 //从Stores中引入
 import abcStore from './stores'
@@ -22,6 +23,7 @@ class App extends Component {
             <Route path="/home" component={Home} />
             <Route path="/detail/:id" component={Detail} />
             <Route path="/hierarchy" component={Hierarchy} />
+            <Route path="/dashboard" component={Dashboard} />
           </Switch>
         </Provider>
       </div>
